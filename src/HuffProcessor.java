13b47b9593c889815d14c9915b81f9b5857b683a
1exG <alexght1@gmail.com>
2018-12-05 00:37:24 -0500

    finished

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -54,15 +54,14 @@ public class HuffProcessor {
 		in.reset();
 		writeCompressedBits(codings,in,out);
 		out.close();
-		
-//		while (true){
-//			int val = in.readBits(BITS_PER_WORD);
-//			if (val == -1) break;
-//			out.writeBits(BITS_PER_WORD, val);
-//		}
 	}
 	
-	
+	/**
+	 * Write compressed bits 
+	 * @param encodings encodings of characters
+	 * @param in input
+	 * @param out output
+	 */
 	private void writeCompressedBits(String[] encodings, BitInputStream in, BitOutputStream out) {
 		
 		while (true){
